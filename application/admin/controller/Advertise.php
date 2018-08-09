e1fab8240791aa7574c1e02471581ac8bba1ec18
wjl <1101452900@qq.com>
2018-08-09 16:08:16 +0800

    人员管理详情页 招聘信息管理详情页

--- application/admin/controller/Advertise.php
+++ application/admin/controller/Advertise.php
@@ -11,5 +11,30 @@ class Advertise extends Controller
     // 方法黑名单
     protected static $blacklist = [];
 
-    
+      /**
+     * 详情
+     */
+    public function detail()
+    {
+        $id = $this->request->param('id');
+        if (!$id) {
+            throw new Exception('缺少必要参数ID');
+        }
+
+        // 条件
+        $map['w.id'] = $id;
+
+        // 查询字段
+        $field = 'w.*';
+
+        // 查询
+        $item = db('Advertise')->alias('w')->field($field)
+         
+            ->where($map)->find();
+
+
+        $this->view->assign('vo', $item);
+
+        return $this->view->fetch();
+    }
 }
--- application/admin/controller/Perman.php
+++ application/admin/controller/Perman.php
@@ -12,4 +12,30 @@ class Perman extends Controller
     protected static $blacklist = [];
 
     
+     /**
+     * 详情
+     */
+    public function detail()
+    {
+        $id = $this->request->param('id');
+        if (!$id) {
+            throw new Exception('缺少必要参数ID');
+        }
+
+        // 条件
+        $map['w.id'] = $id;
+
+        // 查询字段
+        $field = 'w.*';
+
+        // 查询
+        $item = db('Perman')->alias('w')->field($field)
+         
+            ->where($map)->find();
+
+
+        $this->view->assign('vo', $item);
+
+        return $this->view->fetch();
+    }
 }
